12b7695ff2f9d64045b83b27d2a127f328309337
nlacave <lacavenicolasleonel@gmail.com>
2024-06-29 16:52:43 -0300

    Juego de Adivinar el Número Terminado

---
+++ base-juego-adivinanza-js-master/script.js
@@ -0,0 +1,47 @@
+let numeroRandom = Math.trunc(Math.random() * 100) + 1;
+console.log(numeroRandom);
+let entrada = document.getElementById("numeroEntrada");
+let reload = document.getElementById("comprobar");
+let mensaje = document.getElementById("mensaje");
+let intentos = document.getElementById("intentos");
+let contadorIntentos = 0;
+let juegoGanado = false;
+intentos.textContent = contadorIntentos;
+mensaje.textContent = "A jugar!";
+
+
+const comprobar = () => {
+    if(!juegoGanado) {
+        entrada.value = Math.trunc(entrada.value);
+        contadorIntentos++;
+        intentos.textContent = contadorIntentos;
+        mensaje.style.color = "red";
+        if(entrada.value == numeroRandom) {
+            mensaje.textContent = "Felicitaciones! Has adivinado el número.";
+            mensaje.style.color = "green";
+            juegoGanado = true;
+            reload.textContent = "Volver a jugar.";
+        } else if(entrada.value >= 1 && entrada.value <= 100 && entrada.value != numeroRandom) {
+            if(entrada.value < numeroRandom) {
+                mensaje.textContent = "Que lastima! El número a adivinar es más alto. Vuelve a intentarlo.";
+            } else {
+                mensaje.textContent = "Que lastima! El número a adivinar es más bajo. Vuelve a intentarlo.";
+        }
+    } else {
+            mensaje.textContent = "Ingresaste un número fuera de rango o un elemento erroneo. Vuelve a intentarlo.";
+        }
+    } else if(juegoGanado && reload.onclick) {
+        reiniciarJuego();
+    }
+}
+
+const reiniciarJuego = () => {
+    numeroRandom = Math.trunc(Math.random() * 100) + 1;
+    console.log(numeroRandom);
+    entrada.value = "";
+    mensaje.innerHTML = "A jugar!";
+    reload.innerHTML = "Comprobar";
+    contadorIntentos = 0;
+    intentos.textContent = contadorIntentos;
+    juegoGanado = false;
+}
