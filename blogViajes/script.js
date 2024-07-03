09e7111ffb68cb55b5da1a98b2a0fbf9dadc9d23
Nicolas Lacave <Nicolas.Lacave@belltech.la>
2024-07-03 17:03:40 -0300

    Agregada pagina de viajes

--- blogViajes/script.js
+++ blogViajes/script.js
@@ -1,16 +1,35 @@
-import {barcelona, roma, paris, londres} from "./ciudades.js";
+import { barcelona, roma, paris, londres} from "./ciudades.js";
 
-let ciudades = {barcelona, roma, paris, londres}
+let elementos = document.querySelectorAll("a");
 
-globalThis.mostrarInfo = (event) => {
-    const idCiudad = event.currentTarget.id;
-    if(ciudades.hasOwnProperty(idCiudad)) {
-        const ciudad = ciudades[idCiudad];
-        document.getElementById("titulo").innerText = ciudad.titulo;
-        document.getElementById("subtitulo").innerText = ciudad.subtitulo;
-        document.getElementById("parrafo").innerText = ciudad.parrafo;
-    } else {
-        console.error("Ciudad no encontrada:", idCiudad);
-    }
+elementos.forEach(elemento => {
+    elemento.addEventListener("click", function(){
+        elementos.forEach(elemento => {
+            elemento.classList.remove("active");
+        })
+        this.classList.add("active");
+
+        const ciudad = obtenerElemento(elemento.textContent);
+
+        let elementoTitulo = document.getElementById("titulo");
+        let elementoSubtitulo = document.getElementById("subtitulo");
+        let elementoParrafo = document.getElementById("parrafo");
+        let elementoPrecio = document.getElementById("precio");
+
+        elementoTitulo.innerHTML = ciudad.titulo;
+        elementoSubtitulo.innerHTML = ciudad.subtitulo;
+        elementoParrafo.innerHTML = ciudad.parrafo;
+        elementoPrecio.innerHTML = `<h3>${ciudad.precio}</h3>`;
+    })
+})
+
+const obtenerElemento = (elemento) => {
+    const ciudades = {
+        "Barcelona": barcelona,
+        "Roma": roma,
+        "París": paris,
+        "Londres": londres,
+    };
+
+    return ciudades[elemento];
 }
-
